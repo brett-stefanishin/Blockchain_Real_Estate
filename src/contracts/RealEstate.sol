1c2c41705e81eb237fd829f1e051a3f66658de4e
= <=>
2019-10-01 20:02:10 -0400

    Changed smart contract code to incorporate ERC721Full.sol

--- src/contracts/RealEstate.sol
+++ src/contracts/RealEstate.sol
@@ -3,10 +3,16 @@ pragma solidity ^0.5.1;
 import "./ERC721Full.sol";
 
 contract RealEstate is ERC721Full {
+  string[] public tokens;
+  mapping(string => bool) _tokenExists;
 
-  string public name;
+  constructor() ERC721Full("Blockchain Real Estate Token", "BRET") public {
+  }
 
-  constructor () public {
-    name = "Blockchain Real Estate";
+  function mint(string memory _name) public {
+    require(!_tokenExists[_name]);
+    uint _id = tokens.push(_name);
+    _mint(msg.sender, _id);
+    _tokenExists[_name] = true;
   }
 }
